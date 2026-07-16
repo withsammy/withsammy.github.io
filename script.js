64173d3116ff68d6cd331774044789dda465a39c
saemilim <saemi.lim.03@gmail.com>
2026-07-16 14:35:47 +0900

    Initial responsive profile shell

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const siteNav = document.querySelector('.site-nav');
+
+if (menuToggle && siteNav) {
+  menuToggle.addEventListener('click', () => {
+    const isOpen = siteNav.classList.toggle('is-open');
+    menuToggle.setAttribute('aria-expanded', String(isOpen));
+  });
+}
